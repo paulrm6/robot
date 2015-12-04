061226a218942b1ab7070e6bc6d0c06d8e143598
Paul MacDonald <paulrm6@gmail.com>
2015-12-04 03:41:21 +0000

    Increase resolution

--- src/main/robot/Restaurant.java
+++ src/main/robot/Restaurant.java
@@ -1,6 +1,7 @@
 package robot;
 
 import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.util.texture.Texture;
 
 /**
  * Created by Paul on 10/11/2015.
@@ -10,9 +11,12 @@ import com.jogamp.opengl.GL2;
  */
 
 public class Restaurant {
-    private final int POINTS_X = 200, POINTS_Y = 200;
+    private final int POINTS_X = 100, POINTS_Y = 100;
     private final double TILES_X = 1, TILES_Y = 1;
     private double height, width, depth;
+    private Mesh meshSides, meshEnds, meshTB;
+    private RenderMesh sides, ends, top, bottom;
+    private Texture wallpaper, ceiling, floor;
 
     public Restaurant(GL2 gl, double h, double w, double d) {
         height = h;
@@ -21,8 +25,56 @@ public class Restaurant {
     }
 
     public void create(GL2 gl) {
+        wallpaper = RobotRestaurantScene.loadTexture(gl, "wallpaper.jpg");
+        floor = RobotRestaurantScene.loadTexture(gl, "woodFloor.jpg");
+        ceiling = RobotRestaurantScene.loadTexture(gl, "ceiling.jpg");
+        meshSides = ProceduralMeshFactory.createPlane(height,depth,POINTS_X,POINTS_Y,TILES_X,TILES_Y);  // Create the mesh structure
+        sides = new RenderMesh(meshSides, wallpaper);    // Create a new Render object for the mesh
+        sides.initialiseDisplayList(gl, true);
+        meshEnds = ProceduralMeshFactory.createPlane(width,height,POINTS_X,POINTS_Y,TILES_X,TILES_Y);  // Create the mesh structure
+        ends = new RenderMesh(meshEnds, wallpaper);    // Create a new Render object for the mesh
+        ends.initialiseDisplayList(gl, true);
+        meshTB = ProceduralMeshFactory.createPlane(width,depth,POINTS_X,POINTS_Y,TILES_X,TILES_Y);  // Create the mesh structure
+        top = new RenderMesh(meshTB, ceiling);    // Create a new Render object for the mesh
+        top.initialiseDisplayList(gl, true);
+        bottom = new RenderMesh(meshTB, floor);    // Create a new Render object for the mesh
+        bottom.initialiseDisplayList(gl, true);
     }
 
     public void render(GL2 gl) {
+        //left wall
+        gl.glPushMatrix();
+            gl.glTranslated(-width/2,height/2,0);
+            gl.glRotated(270,0,0,1);
+            sides.renderDisplayList(gl);
+        gl.glPopMatrix();
+        //right wall
+        gl.glPushMatrix();
+            gl.glTranslated(width/2,height/2,0);
+            gl.glRotated(90,0,0,1);
+            sides.renderDisplayList(gl);
+        gl.glPopMatrix();
+        //back wall
+        gl.glPushMatrix();
+            gl.glTranslated(0,height/2,-depth/2);
+            gl.glRotated(90,1,0,0);
+            ends.renderDisplayList(gl);
+        gl.glPopMatrix();
+        //front wall
+        gl.glPushMatrix();
+            gl.glTranslated(0,height/2,depth/2);
+            gl.glRotated(270,1,0,0);
+            ends.renderDisplayList(gl);
+        gl.glPopMatrix();
+        //floor
+        gl.glPushMatrix();
+            bottom.renderDisplayList(gl);
+        gl.glPopMatrix();
+        //ceiling
+        gl.glPushMatrix();
+            gl.glTranslated(0,height,0);
+            gl.glRotated(180,1,0,0);
+            top.renderDisplayList(gl);
+        gl.glPopMatrix();
     }
 }
