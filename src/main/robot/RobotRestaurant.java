10335ab883eb905becf7e509ba8ebe554eb0d5d9
Paul MacDonald <paulrm6@gmail.com>
2015-12-06 19:46:11 +0000

    Refactor and clean up

--- src/main/robot/RobotRestaurant.java
+++ src/main/robot/RobotRestaurant.java
@@ -12,9 +12,15 @@ import java.awt.event.*;
  * I declare that this code is my own work.
  * prmacdonald1@sheffield.ac.uk
  * 1350155458
+ *
+ * Modelled off of the Lab class examples provided by
+ * Dr Steve Maddock.
+ *
+ * A class to start the program and control the interface
  */
 
 public class RobotRestaurant extends Frame implements GLEventListener, ActionListener, ItemListener, MouseMotionListener {
+    //Create some variables
     public static final int FRAMES_PER_SEC = 30;
     private static final int WINDOW_WIDTH = 1000;
     private static final int WINDOW_HEIGHT = 1000;
@@ -28,12 +34,16 @@ public class RobotRestaurant extends Frame implements GLEventListener, ActionLis
     private boolean withWorldLighting = true, withRobotLight = true, robotPerspective = false, withSpotlight = true;
     private boolean continuousAnimation;
 
+    /**
+     * Constructor
+     */
     public RobotRestaurant() {
         //Set title of window
         super("Paul's Robot Restaurant");
         //Set size of the window
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
+        //Set the capabilities and create the canvas
         GLProfile glp = GLProfile.getDefault();
         GLCapabilities caps = new GLCapabilities(glp);
         canvas = new GLCanvas(caps);
@@ -88,16 +98,16 @@ public class RobotRestaurant extends Frame implements GLEventListener, ActionLis
         //Add a event listener to the canvas
         canvas.addGLEventListener(this);
         //Create and start an animator
-        FPSAnimator animator = new FPSAnimator(canvas, FRAMES_PER_SEC);
-        animator.start();
-        canvas.addMouseMotionListener(this);
+        FPSAnimator animator = new FPSAnimator(canvas, FRAMES_PER_SEC);//Create the animator
+        animator.start(); //Start the animator
+        canvas.addMouseMotionListener(this);//Listen for mouse events on the canvas
     }
 
-    public static void main(String[] args) {
-        RobotRestaurant robotRestaurant = new RobotRestaurant();
-        robotRestaurant.setVisible(true);
-    }
-
+    /**
+     * Method defined by GLEventListener.
+     * Responds to events such as button clicks
+     * @param e Automatically supplied by the system when an action occurs
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         //If user clicks quit in File->Quit
@@ -128,6 +138,11 @@ public class RobotRestaurant extends Frame implements GLEventListener, ActionLis
         }
     }
 
+    /**
+     * Method defined by GLEventListener
+     * Respons to changed to any Checkbox in the GUI
+     * @param e Automatically supplied by the system when a checkbox changes it's state
+     */
     @Override
     public void itemStateChanged(ItemEvent e) {
         Object source = e.getSource();
@@ -151,32 +166,50 @@ public class RobotRestaurant extends Frame implements GLEventListener, ActionLis
         }
     }
 
+    /**
+     * Methods defined by GLEventListener
+     * Initialises the OpenGL context
+     * @param glAutoDrawable drawable automatically supplied by the system
+     */
     @Override
     public void init(GLAutoDrawable glAutoDrawable) {
         GL2 gl = glAutoDrawable.getGL().getGL2();
         gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f); //black background
-        gl.glEnable(GL.GL_DEPTH_TEST);
-        gl.glDisable(GL.GL_CULL_FACE);
-        gl.glFrontFace(GL.GL_CCW);
-        gl.glCullFace(GL.GL_BACK);
-        gl.glShadeModel(GL2.GL_SMOOTH);
-        gl.glPolygonMode(GL.GL_FRONT_AND_BACK, GL2.GL_FILL);
-        gl.glEnable(GL2.GL_LIGHTING);
-        gl.glEnable(GL2.GL_NORMALIZE);
+        gl.glEnable(GL.GL_DEPTH_TEST);//Enable depth testing using z-buffer
+        gl.glDisable(GL.GL_CULL_FACE);//Disable face culling so no polygons are discarded
+        gl.glCullFace(GL.GL_BACK);//For when face culling is enabled, discard polygons facing away from the camera
+        gl.glShadeModel(GL2.GL_SMOOTH);//Colours computed at vertices are interpolated over the surface
+        gl.glPolygonMode(GL.GL_FRONT_AND_BACK, GL2.GL_FILL);//Front and back facing polygons should be filled
+        gl.glEnable(GL2.GL_LIGHTING);//Lighting will be used
+        gl.glEnable(GL2.GL_NORMALIZE);//Vectors with glNormal are scaled to unit length after transformation
         double radius = 50.0; //distance of the camera from world origin
         double theta = Math.toRadians(-90); // theta rotates anticlockwise around y axis
         double phi = Math.toRadians(5);  // phi is inclination from ground plane
-        camera = new Camera(theta, phi, radius);
-        scene = new RobotRestaurantScene(gl, camera);
+        camera = new Camera(theta, phi, radius); //initiate a camera
+        scene = new RobotRestaurantScene(gl, camera); //initiate a scene
     }
 
+    /**
+     * Method defined by GLEventListener
+     * Called when the canvas is displayed
+     * @param glAutoDrawable drawable automatically supplied by the system
+     */
     @Override
     public void display(GLAutoDrawable glAutoDrawable) {
         GL2 gl = glAutoDrawable.getGL().getGL2();
-        if (continuousAnimation) scene.update();
-        scene.render(gl, withWorldLighting, withRobotLight, robotPerspective, withSpotlight);
+        if (continuousAnimation) scene.update(); //If continuous animation is enabled, update the scene
+        scene.render(gl, withWorldLighting, withRobotLight, robotPerspective, withSpotlight); //Render the scene
     }
 
+    /**
+     * Method defined by GLEventListener
+     * Called when the user resizes the window
+     * @param glAutoDrawable drawable automatically supplied by the system
+     * @param x x coordinate of top left of window (supplied by system)
+     * @param y y coordinate of top left of window (supplied by system)
+     * @param width width of the window (supplied by system)
+     * @param height height of the window (supplied by system)
+     */
     @Override
     public void reshape(GLAutoDrawable glAutoDrawable, int x, int y, int width, int height) {
         GL2 gl = glAutoDrawable.getGL().getGL2();
@@ -197,6 +230,11 @@ public class RobotRestaurant extends Frame implements GLEventListener, ActionLis
         gl.glMatrixMode(GL2.GL_MODELVIEW);
     }
 
+    /**
+     * Method defined by GLEventListener
+     * Called when closing the openGL context
+     * @param glAutoDrawable drawable automatically supplied by the system
+     */
     @Override
     public void dispose(GLAutoDrawable glAutoDrawable) {
 
@@ -229,5 +267,10 @@ public class RobotRestaurant extends Frame implements GLEventListener, ActionLis
      */
     public void mouseMoved(MouseEvent e) {
         lastpoint = e.getPoint();
+    }
+
+    public static void main(String[] args) {
+        RobotRestaurant robotRestaurant = new RobotRestaurant();
+        robotRestaurant.setVisible(true);
     }
 }
--- src/main/robot/RobotWaiter.java
+++ src/main/robot/RobotWaiter.java
@@ -9,9 +9,12 @@ import com.jogamp.opengl.util.gl2.GLUT;
  * I declare that this code is my own work.
  * prmacdonald1@sheffield.ac.uk
  * 1350155458
+ *
+ * A class the draw the robot
  */
 
 public class RobotWaiter {
+    //Create some variables
     private final float[] AMBIENT = {0.2f, 0.2f, 0.2f, 1.0f}; //dark grey
     private final float[] ROBOT_COLOUR = {0.8f, 0.8f, 0.8f, 1.0f}; //grey
     private final int RESOLUTION = 100;
@@ -21,210 +24,279 @@ public class RobotWaiter {
     private double sideTilt = 0, frontTilt = 0;
     private double headTilt = 0;
     private double trayArm = 0;
-
     private boolean perspective;
 
     public RobotWaiter() {
     }
 
+    /**
+     * A method to draw the robot
+     * @param gl openGL context
+     * @param glut glut
+     * @param withRobotLight boolean, true if the robot light is on
+     */
     public void draw(GL2 gl, GLUT glut, boolean withRobotLight) {
+        //Set some material properties
         gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_AMBIENT, AMBIENT, 0);
         gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_DIFFUSE, ROBOT_COLOUR, 0);
 
         gl.glPushMatrix();
-        gl.glTranslated(x, 0, z);
-        gl.glRotated(rotate, 0, 1, 0);
-        gl.glRotated(270, 0, 1, 0);
-        drawCone(gl, glut, 1, 1.2);
-        gl.glTranslated(0, 1, 0);
-        gl.glPushMatrix();
-        gl.glRotated(frontTilt, 0, 0, 1);
-        gl.glRotated(sideTilt, 1, 0, 0);
-        drawCylinder(gl, glut, 2.5, 0.7); //draw body
-        //draw left arm
-        drawArm(gl, glut, true, -15, (-60 - sideTilt), true);
-        //draw right arm
-        drawArm(gl, glut, false, -50, (-40 - sideTilt), false);
-        if (!perspective) {
-            //draw neck
+            gl.glTranslated(x, 0, z);//change robot position
+            gl.glRotated(rotate, 0, 1, 0);//change robot direction
+            gl.glRotated(270, 0, 1, 0);
+            drawCone(gl, glut, 1, 1.2);//draw the base
+            gl.glTranslated(0, 1, 0);//move everything up by 1
             gl.glPushMatrix();
-            gl.glTranslated(0.15, 2.5, 0);
-            drawCylinder(gl, glut, 0.2, 0.2);
-            //draw head
-            gl.glPushMatrix();
-            gl.glTranslated(0, 0.2, 0);
-            gl.glRotated(headTilt, 0, 0, 1);
-            drawRectangle(gl, glut, 1, 0.9, 0.9, 0);
-            //set eye emission
-            if (withRobotLight) {
-                float[] yellowish = {1.0f, 1.0f, 0.6f, 1.0f};
-                gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_EMISSION, yellowish, 0);
-            }
-            //draw left eye
-            gl.glTranslated(0.45, 0.7, 0);
-            gl.glPushMatrix();
-            gl.glTranslated(0, 0, 0.2);
-            glut.glutSolidSphere(0.1, RESOLUTION, RESOLUTION);
+                gl.glRotated(frontTilt, 0, 0, 1);//Add tilt to the front
+                gl.glRotated(sideTilt, 1, 0, 0);//Add tilt to the side
+                drawCylinder(gl, glut, 2.5, 0.7); //draw body
+                drawArm(gl, glut, true, -15, (-60 - sideTilt), true);//draw left arm
+                drawArm(gl, glut, false, -50, (-40 - sideTilt), false);//draw right arm
+                if (!perspective) { //if we're not looking from the robot perspective
+                    //draw neck
+                    gl.glPushMatrix();
+                        gl.glTranslated(0.15, 2.5, 0);
+                        drawCylinder(gl, glut, 0.2, 0.2);
+                        //draw head
+                        gl.glPushMatrix();
+                            gl.glTranslated(0, 0.2, 0);
+                            gl.glRotated(headTilt, 0, 0, 1);//add head tilt
+                            drawRectangle(gl, glut, 1, 0.9, 0.9, 0);
+                            //set eye emission if robot light is on
+                            if (withRobotLight) {
+                                float[] yellowish = {1.0f, 1.0f, 0.6f, 1.0f};
+                                gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_EMISSION, yellowish, 0);
+                            }
+                            gl.glTranslated(0.45, 0.7, 0);
+                            gl.glPushMatrix();//draw left eye
+                                gl.glTranslated(0, 0, 0.2);
+                                glut.glutSolidSphere(0.1, RESOLUTION, RESOLUTION);
+                            gl.glPopMatrix();
+                            gl.glPushMatrix();//draw right eye
+                                gl.glTranslated(0, 0, -0.2);
+                                glut.glutSolidSphere(0.1, RESOLUTION, RESOLUTION);
+                            gl.glPopMatrix();
+                        gl.glPopMatrix();
+                    gl.glPopMatrix();
+                }
             gl.glPopMatrix();
-            //draw right eye
-            gl.glPushMatrix();
-            gl.glTranslated(0, 0, -0.2);
-            glut.glutSolidSphere(0.1, RESOLUTION, RESOLUTION);
-            gl.glPopMatrix();
-            gl.glPopMatrix();
-            gl.glPopMatrix();
-        }
         gl.glPopMatrix();
-        gl.glPopMatrix();
-
     }
 
+    /**
+     * A method to draw an arm
+     * @param gl openGL context
+     * @param glut glut
+     * @param leftSide true if arm is on the left
+     * @param rotateArm angle of upper arm rotate
+     * @param rotateForearm angle of lower arm rotate
+     * @param closedHand true if hand is closed
+     */
     private void drawArm(GL2 gl, GLUT glut, boolean leftSide, double rotateArm, double rotateForearm, boolean closedHand) {
         double angle;
-        int side;
-        if (leftSide) {
+        int side,clawAngle;
+        if (leftSide) {//if left arm (tray arm)
             angle = 90;
             side = 1;
             rotateArm = rotateArm + 0.5 * trayArm;
-        } else {
+        } else {//if right arm
             angle = 270;
             side = -1;
         }
-        int clawAngle;
-        if (closedHand) {
+        if (closedHand) {//If hand is closed
             clawAngle = -5;
-        } else {
+        } else {//If hand is open
             clawAngle = 10;
         }
         gl.glPushMatrix();
-        gl.glTranslated(0, 2, side * 0.6);
-        if (leftSide) {
-            gl.glRotated(trayArm - frontTilt, 0, 0, 1);
-        }
-        gl.glRotated(rotateArm, 1, 0, 0); // rotate arm
-        drawRectangle(gl, glut, 1, 0.25, 0.25, angle);
-        //draw forearm
-        gl.glPushMatrix();
-        gl.glTranslated(0, 0, side * 0.9);
-        gl.glRotated(rotateForearm, 1, 0, 0); // rotate forearm
-        drawRectangle(gl, glut, 1, 0.2, 0.2, angle);
-        //draw claw
-        gl.glTranslated(0, 0, side * 0.95);
-        gl.glPushMatrix();
-        gl.glTranslated(0, side * 0.05, 0);
-        gl.glRotated(clawAngle, 1, 0, 0);
-        drawRectangle(gl, glut, 0.3, 0.05, 0.05, angle);
-        gl.glPopMatrix();
-        gl.glPushMatrix();
-        gl.glTranslated(0, -side * 0.05, 0);
-        gl.glRotated(-clawAngle, 1, 0, 0);
-        drawRectangle(gl, glut, 0.3, 0.05, 0.05, angle);
-        gl.glPopMatrix();
-        gl.glPushMatrix();
-        if (leftSide) {
-            gl.glTranslated(0, 0, 0.3);
-            gl.glRotated(rotateArm + rotateForearm + sideTilt, -1, 0, 0);
-            gl.glRotated(trayArm, 0, 0, -1);
-            drawTray(gl, glut);
-        }
-        gl.glPopMatrix();
-        gl.glPopMatrix();
+            gl.glTranslated(0, 2, side * 0.6);
+            if (leftSide) {
+                gl.glRotated(trayArm - frontTilt, 0, 0, 1);
+            }
+            gl.glRotated(rotateArm, 1, 0, 0); // rotate arm
+            drawRectangle(gl, glut, 1, 0.25, 0.25, angle);//draw upper arm
+            gl.glPushMatrix();
+                gl.glTranslated(0, 0, side * 0.9);
+                gl.glRotated(rotateForearm, 1, 0, 0); // rotate forearm
+                drawRectangle(gl, glut, 1, 0.2, 0.2, angle);//draw forearm
+                //draw claws
+                gl.glTranslated(0, 0, side * 0.95);
+                gl.glPushMatrix();
+                    gl.glTranslated(0, side * 0.05, 0);
+                    gl.glRotated(clawAngle, 1, 0, 0);
+                    drawRectangle(gl, glut, 0.3, 0.05, 0.05, angle);//draw claw
+                gl.glPopMatrix();
+                gl.glPushMatrix();
+                    gl.glTranslated(0, -side * 0.05, 0);
+                    gl.glRotated(-clawAngle, 1, 0, 0);
+                    drawRectangle(gl, glut, 0.3, 0.05, 0.05, angle);//draw other claw
+                gl.glPopMatrix();
+                if (leftSide) {//If we are on the tray arm
+                    gl.glPushMatrix();
+                        gl.glTranslated(0, 0, 0.3);
+                        gl.glRotated(rotateArm + rotateForearm + sideTilt, -1, 0, 0);//Keep it horizontal
+                        gl.glRotated(trayArm, 0, 0, -1);//Keep it horizontal
+                        drawTray(gl, glut);//Draw the tray
+                    gl.glPopMatrix();
+                }
+            gl.glPopMatrix();
         gl.glPopMatrix();
     }
 
+    /**
+     * Draw the tray
+     * @param gl openGL context
+     * @param glut glut
+     */
+    private void drawTray(GL2 gl, GLUT glut) {
+        gl.glPushMatrix();
+            //change material for the tray
+            gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_DIFFUSE, new float[]{0, 0, 0, 1}, 0);//black
+            drawCylinder(gl, glut, 0.1, 0.7);//draw tray
+            gl.glTranslated(0, 0.1, 0);
+            //change material for the objects
+            gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_DIFFUSE, new float[]{0.7f, 0.7f, 1, 1}, 0);//blueish tinge
+            gl.glPushMatrix();//draw glass
+                gl.glTranslated(0.2, 0, -0.2);
+                drawCylinder(gl, glut, 0.3, 0.05);
+                gl.glTranslated(0, 0.5, 0);
+                gl.glRotated(180, 0, 0, 1);
+                drawCone(gl, glut, 0.2, 0.3);
+            gl.glPopMatrix();
+            gl.glPushMatrix();//draw can
+                gl.glTranslated(0, 0, 0.2);
+                drawCylinder(gl, glut, 0.3, 0.1);
+            gl.glPopMatrix();
+            gl.glPushMatrix();//draw box
+                gl.glTranslated(-0.2, 0, 0);
+                drawRectangle(gl, glut, 0.4, 0.2, 0.2, 0);
+            gl.glPopMatrix();
+            //change material back
+            gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_DIFFUSE, ROBOT_COLOUR, 0);//grey
+        gl.glPopMatrix();
+    }
+
+    /**
+     * Draw a cone
+     * @param gl openGL context
+     * @param glut glut
+     * @param h height of the cone
+     * @param r radius of the cone
+     */
     private void drawCone(GL2 gl, GLUT glut, double h, double r) {
         gl.glPushMatrix();
-        gl.glRotated(270, 1, 0, 0);
-        glut.glutSolidCone(h, r, RESOLUTION, RESOLUTION);
+            gl.glRotated(270, 1, 0, 0);
+            glut.glutSolidCone(h, r, RESOLUTION, RESOLUTION);//draw cone
         gl.glPopMatrix();
     }
 
-    private void drawTray(GL2 gl, GLUT glut) {
-        gl.glDisable(GL.GL_CULL_FACE);
-        gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_DIFFUSE, new float[]{0, 0, 0, 1}, 0);//black
-        drawCylinder(gl, glut, 0.1, 0.7);
-        gl.glTranslated(0, 0.1, 0);
-        gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_DIFFUSE, new float[]{0.7f, 0.7f, 1, 1}, 0);//blueish tinge
-        gl.glPushMatrix();
-        gl.glTranslated(0.2, 0, -0.2);
-        drawCylinder(gl, glut, 0.3, 0.05);
-        gl.glTranslated(0, 0.5, 0);
-        gl.glRotated(180, 0, 0, 1);
-        drawCone(gl, glut, 0.2, 0.3);
-        gl.glPopMatrix();
-        gl.glPushMatrix();
-        gl.glTranslated(0, 0, 0.2);
-        drawCylinder(gl, glut, 0.3, 0.1);
-        gl.glPopMatrix();
-        gl.glPushMatrix();
-        gl.glTranslated(-0.2, 0, 0);
-        drawRectangle(gl, glut, 0.4, 0.2, 0.2, 0);
-        gl.glPopMatrix();
-        gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_DIFFUSE, ROBOT_COLOUR, 0);//grey
-        gl.glEnable(GL.GL_CULL_FACE);
-    }
-
+    /**
+     * Draw a rectangle
+     * @param gl openGL context
+     * @param glut glut
+     * @param l length of rectangle
+     * @param w width of rectangle
+     * @param d depth of rectangle
+     * @param r rotation of rectangle
+     */
     private void drawRectangle(GL2 gl, GLUT glut, double l, double w, double d, double r) {
         gl.glPushMatrix();
-        gl.glRotated(r, 1, 0, 0);
-        gl.glScaled(w, l, d);
-        gl.glTranslated(0, 0.5, 0);
-        glut.glutSolidCube(1);
+            gl.glRotated(r, 1, 0, 0);
+            gl.glScaled(w, l, d);
+            gl.glTranslated(0, 0.5, 0);
+            glut.glutSolidCube(1);
         gl.glPopMatrix();
     }
 
+    /**
+     * Draw a cylinder
+     * @param gl openGl context
+     * @param glut glut
+     * @param h height of cylinder
+     * @param r radius of cylinder
+     */
     private void drawCylinder(GL2 gl, GLUT glut, double h, double r) {
         gl.glPushMatrix();
-        gl.glScaled(r, h, r);
-        gl.glRotated(270, 1, 0, 0);
-        glut.glutSolidCylinder(1, 1, RESOLUTION, RESOLUTION);
+            gl.glScaled(r, h, r);
+            gl.glRotated(270, 1, 0, 0);
+            glut.glutSolidCylinder(1, 1, RESOLUTION, RESOLUTION);
         gl.glPopMatrix();
     }
 
+    /**
+     * @return x
+     */
     public double getX() {
         return x;
     }
 
+    /**
+     * @param x set x value
+     */
     public void setX(double x) {
         this.x = x;
     }
 
+    /**
+     * @return z
+     */
     public double getZ() {
         return z;
     }
 
+    /**
+     * @param z set z value
+     */
     public void setZ(double z) {
         this.z = z;
     }
 
-    public double getRotate() {
-        return rotate;
-    }
-
+    /**
+     * @param rotate set rotate value (degrees)
+     */
     public void setRotate(double rotate) {
         this.rotate = Math.toDegrees(rotate);
     }
 
+    /**
+     * @return lookX (radians)
+     */
     public double getLookX() {
         return Math.sin(Math.toRadians(rotate));
     }
 
+    /**
+     * @return lookZ (radians)
+     */
     public double getLookZ() {
         return Math.cos(Math.toRadians(rotate));
     }
 
+    /**
+     * @param headTilt set tilt of head (degrees)
+     */
     public void setHeadTilt(double headTilt) {
         this.headTilt = headTilt;
     }
 
+    /**
+     * @param trayArm set angle of tray arm (degrees)
+     */
     public void setTrayArm(double trayArm) {
         this.trayArm = trayArm;
     }
 
+    /**
+     * @param perspective set the perspective
+     */
     public void setPerspective(boolean perspective) {
         this.perspective = perspective;
     }
 
+    /**
+     * @param front set front tilt
+     * @param side set side tilt
+     */
     public void setRobotTilt(double front, double side) {
         frontTilt = front;
         sideTilt = side;
